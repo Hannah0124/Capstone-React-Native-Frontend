bbbd8c70ad4dcbdabe2a6acae574933797fc01b5
Sharon Cheung <keikei1128@g.ucla.edu>
2020-07-06 11:00:19 -0700

    set up basic homescreen

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MainStackNavigator from './src/navigation/MainStackNavigator'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <MainStackNavigator />
 }
 
 const styles = StyleSheet.create({
