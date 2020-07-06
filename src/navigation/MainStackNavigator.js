e029ec43c1c1bc4250f80a48c3333e958c821f32
Sharon Cheung <keikei1128@g.ucla.edu>
2020-07-06 11:28:24 -0700

    basic stack navigator

--- src/navigation/MainStackNavigator.js
+++ src/navigation/MainStackNavigator.js
@@ -3,14 +3,29 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 
 import Home from '../screens/Home'
+import PhotoTranslator from '../screens/PhotoTranslator'
 
 const Stack = createStackNavigator()
 
 function MainStackNavigator() {
   return (
-    <NavigationContainer>
+    <NavigationContainer initialRouteName='Home'>
       <Stack.Navigator>
-        <Stack.Screen name='Home' component={Home} />
+      <Stack.Screen
+          name='Home'
+          component={Home}
+          options={{ title: 'Home Screen' }}
+        />
+        <Stack.Screen
+          name='PhotoTranslator'
+          component={PhotoTranslator}
+          options={{ title: 'Translate things by taking a photo!' }}
+        />
+        <Stack.Screen
+          name='WordTranslator'
+          component={PhotoTranslator}
+          options={{ title: 'Translate them in different languages!' }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   )
