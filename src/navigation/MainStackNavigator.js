aba885f7b0a353a2c9811206315a69fa8bf2f1ff
Sharon Cheung <keikei1128@g.ucla.edu>
2020-07-06 12:08:49 -0700

    added two buttons in front page

--- src/navigation/MainStackNavigator.js
+++ src/navigation/MainStackNavigator.js
@@ -1,15 +1,31 @@
+// navigation reference from :
+// https://heartbeat.fritz.ai/getting-started-with-stack-navigator-using-react-navigation-5-in-react-native-and-expo-apps-4c516becaee1
 import * as React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 
 import Home from '../screens/Home'
 import PhotoTranslator from '../screens/PhotoTranslator'
+import WordTranslator from '../screens/WordTranslator'
 
 const Stack = createStackNavigator()
 
 function MainStackNavigator() {
   return (
-    <NavigationContainer initialRouteName='Home'>
+    <NavigationContainer  
+      initialRouteName='Home'
+      screenOptions={{
+        gestureEnabled: true,
+        headerStyle: {
+          backgroundColor: '#101010'
+        },
+        headerTitleStyle: {
+          fontWeight: 'bold'
+        },
+        headerTintColor: '#ffd700',
+        headerBackTitleVisible: false
+      }}
+      headerMode='float'>
       <Stack.Navigator>
       <Stack.Screen
           name='Home'
@@ -19,12 +35,12 @@ function MainStackNavigator() {
         <Stack.Screen
           name='PhotoTranslator'
           component={PhotoTranslator}
-          options={{ title: 'Translate things by taking a photo!' }}
+          options={{ title: 'Picture worth a thousand words' }}
         />
         <Stack.Screen
           name='WordTranslator'
-          component={PhotoTranslator}
-          options={{ title: 'Translate them in different languages!' }}
+          component={WordTranslator}
+          options={{ title: 'No more Language Barrier' }}
         />
       </Stack.Navigator>
     </NavigationContainer>
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,22 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, TouchableOpacity} from 'react-native'
+import { Ionicons } from '@expo/vector-icons';
 
 const Home = props =>  {
+  const { navigation } = props
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home Screen</Text>
+      <TouchableOpacity
+        style={styles.buttonContainer}
+        onPress={() => navigation.navigate('WordTranslator')}>
+        <Text style={styles.buttonText}>Translating words</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.buttonContainer}
+        onPress={() => navigation.navigate('PhotoTranslator')}>
+        <Text style={styles.buttonText}>Translating Photo</Text>
+      </TouchableOpacity>
     </View>
   )
 }
@@ -20,6 +32,16 @@ const styles = StyleSheet.create({
     color: '#101010',
     fontSize: 24,
     fontWeight: 'bold'
+  },
+  buttonContainer: {
+    backgroundColor: '#222',
+    borderRadius: 5,
+    padding: 10,
+    margin: 20
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff'
   }
 })
 
