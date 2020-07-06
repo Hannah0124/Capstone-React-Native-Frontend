8de4193d6b153b1236fd5cf99357943701618244
Sharon Cheung <keikei1128@g.ucla.edu>
2020-07-06 15:49:44 -0700

    implement camera function for word translator.js

--- src/screens/WordTranslator.js
+++ src/screens/WordTranslator.js
@@ -1,20 +1,69 @@
-import React from 'react'
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, TextInput, Button, TouchableOpacity, ScrollView } from 'react-native';
+import { useDispatch } from 'react-redux'; // TEST
+
+import Colors from '../constants/Colors';
+import * as imagesActions from '../store/images-actions';
+import ImagePicker from '../components/ImagePicker';
 
 const WordTranslator = (props) => {
-  const { navigation } = props
-  
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Word Translator Content</Text>
 
-      <TouchableOpacity
-        style={styles.buttonContainer}
-        onPress={() => navigation.navigate('Settings')}
-      >
-        <Text style={styles.buttonText}>Go to Settings</Text>
-      </TouchableOpacity>
-    </View>
+  const [titleValue, setTitleValue] = useState('');
+  const [selectedImage, setSelectedImage] = useState();
+
+  const { navigation } = props;
+
+  const dispatch = useDispatch(); // TEST
+
+  // TEST
+  const titleChangeHandler = text => {
+    // You could add validation 
+    setTitleValue(text);
+  };
+
+  // TEST
+  const imageTakenHandler = imagePath => {
+    setSelectedImage(imagePath);
+  };
+
+  // TEST
+  const saveImageHandler = () => {
+    dispatch(imagesActions.addImage(titleValue, selectedImage));
+    navigation.goBack();
+  };
+
+
+
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.text}>Photo Translator Content</Text>
+
+        {/* TEST */}
+        <TextInput 
+          style={styles.textInput} 
+          onChangeText={titleChangeHandler} 
+          value={titleValue}
+        />
+
+        <ImagePicker 
+          onImageTaken={imageTakenHandler} 
+        />
+        <Button 
+          title="Save Image" 
+          color={Colors.primary} 
+          onPress={saveImageHandler}
+        />
+
+        <TouchableOpacity
+          style={styles.buttonContainer}
+          onPress={() => navigation.navigate('Settings')}
+        >
+          <Text style={styles.buttonText}>Go to Settings</Text>
+        </TouchableOpacity>
+      </View>
+
+    </ScrollView>
   )
 }
 
@@ -25,6 +74,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     // backgroundColor: '#747EFD',
     backgroundColor: '#fff',
+    margin: 30
 
   },
   text: {
@@ -32,24 +82,6 @@ const styles = StyleSheet.create({
     color: '#747EFD',
     fontSize: 24,
     fontWeight: 'bold'
-  },
-  card: {
-    width: 350,
-    height: 100,
-    borderRadius: 10,
-    borderWidth: 2,
-    // borderColor: '#fff',
-    borderColor: '#747EFD',
-    opacity: 10,
-    margin: 10,
-    padding: 10,
-    alignItems: 'center'
-  },
-  cardText: {
-    fontSize: 18,
-    // color: '#fff',
-    color: '#747EFD',
-    marginBottom: 5
   },
   buttonContainer: {
     backgroundColor: '#747EFD',
