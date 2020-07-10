9d8435a4d934e7b6119e5134e9c0928a35540a5a
Hannah <hannahjoo24@gmail.com>
2020-07-10 09:39:46 -0700

    Array error (testing)

--- src/screens/List.js
+++ src/screens/List.js
@@ -10,33 +10,48 @@ import * as imagesActions from '../store/images-actions';
 const List = props => {
   const images = useSelector(state => state.images.images);
 
+  console.log('images in List.js: ', images)
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(imagesActions.loadImages());
-  }), [dispatch];
+  
+  const newImages = props.route.params.images;
+  // console.log('images in List.js: ', props.route.params.images)
+
+  const listComponent = newImages.map(image => {
+    return (
+      <ImageItem 
+        image={image.imageUri}
+        text={image.text}
+        translatedText={image.translatedText}
+      />
+    )
+  })
+
+  // useEffect(() => {
+  //   dispatch(imagesActions.loadImages());
+  // }), [dispatch];
 
   return (
-    <FlatList
-      data={images}
-      keyExtractor={item => item.id}
-      renderItem={itemData => (
-        <ImageItem
-          image={itemData.item.imageUri}
-          title={itemData.item.title}
-          text="dummy text"
-          translatedText="dummy text"
-          // text={itemData.item.text}
-          // translatedText={itemData.item.translatedText}
-          onSelect={() => {
-            // props.navigation.navigate('ImageDetail', {
-            //   imageTitle: itemData.item.title,
-            //   imageId: itemData.item.id
-            // });
-          }}
-        />
-      )}
-    />
+    { listComponent }
+    // <FlatList
+    //   data={images}
+    //   keyExtractor={item => item.id}
+    //   // renderItem={itemData => (
+    //   itemData={ listComponent }
+    //     // <ImageItem
+    //     //   image={itemData.item.imageUri}
+    //     //   // title={itemData.item.title}
+    //     //   // text={itemData.item.text}
+    //     //   // translatedText={itemData.item.translatedText}
+    //     //   onSelect={() => {
+    //     //     // props.navigation.navigate('ImageDetail', {
+    //     //     //   imageTitle: itemData.item.title,
+    //     //     //   imageId: itemData.item.id
+    //     //     // });
+    //     //   }}
+    //     // />
+    //   // )}
+    // />
   );
 };
 
