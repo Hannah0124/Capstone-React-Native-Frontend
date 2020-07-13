1353756373e478fecd9eb7bffa59e34aeda18e16
Hannah <hannahjoo24@gmail.com>
2020-07-12 22:53:50 -0700

    Add Urls.js

---
+++ src/constants/Urls.js
@@ -0,0 +1,7 @@
+import ENV from '../../env'; // npm i expo-env
+
+export default {
+  GOOGOLE_VISION_URL: `https://content-vision.googleapis.com/v1/images:annotate?key=${ENV.googleApiKey}`,
+  GOOGOLE_TRANSLATION_URL: `https://translation.googleapis.com/language/translate/v2?key=${ENV.googleApiKey}`,
+  BASE_URL: 'http://192.168.0.38:5000'
+};
