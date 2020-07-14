b895d190b04c11140434738f1a3f34e1821b4241
Hannah <hannahjoo24@gmail.com>
2020-07-13 22:37:55 -0700

    Add removeImageHandler to List.js

--- src/screens/List.js
+++ src/screens/List.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Platform, FlatList } from 'react-native';
+import { View, Text, StyleSheet, Platform, FlatList, Alert } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
@@ -12,16 +12,20 @@ import URLS from '../constants/Urls';
 
 const List = props => {
   // const myImages = props.route.params.myImages;
-  // console.log('params in List.js: ', props.route.params.myImages)
-  // const [images, setImages] = useState([]);
+  console.log('???props in List.js: ', props)
+  // const [myImages, setMyImages] = useState([]);
   // const [errorMessage, setErrorMessage] = useState(null);
+  
 
   
   // TEST: TODO
-  // const images = useSelector(state => state.images.images);
-  const myImages = useSelector(state => props.route.params.myImages);
+  // const myCurrentImages = useSelector(state => props.route.params.myImages);
+  // setMyImages(myCurrentImages);
 
-  // console.log('1. images in List.js!!: ', images);
+  const myImages = useSelector(state => props.route.params.myImages);
+  // const [newImages, setNewImages] = useState([myImages]);
+
+  console.log('myImages from list.js~: ', myImages)
 
   const dispatch = useDispatch();
 
@@ -33,51 +37,74 @@ const List = props => {
   // const newImages = props.route.params.images;
   console.log('2. myImages in List.js: ', myImages)
 
-  const listComponent = myImages.map(image => {
-    return (
-      <ImageItem 
-        imageUri={image.image_url}
-        text={image.text}
-        translatedText={image.translated_text}
-        favorite={image.favorite}
-        language={image.language}
-        uid={image.uid}
-      />
-    )
-  })
 
+  const removeImageHandler = (id) => {
+
+    axios.post(`${URLS.BASE_URL}/image/${id}`)
+      .then(response => {
+        console.log('4. internal API - successfully deleted: ', response.data)
+
+        const filterdMyImages = myImages.filter(image => {
+          return image.id !== id
+        });
+
+        props.route.params.updateImagesCallback(filterdMyImages)
+        // setNewImages(filterdMyImages);
+
+        Alert.alert(
+          "Delete the image",
+          "Are you sure?",
+          [
+            { text: "OK", 
+              onPress: () => console.log("OK Pressed") 
+            }
+          ]
+        )
+
+        props.navigation.navigate('List');
+        // props.navigation.goBack();
+
+      })
+      .catch(err => {
+        console.log('4. internal API - error (deleted): ', err)
+        Alert.alert(
+          "Something went wrong",
+          `Please try again. Error Message: ${err.message}\n`,
+          [
+            { text: "OK", 
+              onPress: () => console.log("OK Pressed") 
+            }
+          ]
+        )
+      })
+  };
   
-
   return (
     <View>
-      {/* <View>
-      {listComponent}
-      </View> */}
-
-
-    <FlatList
-      data={myImages}
-      keyExtractor={item => item.id}
-      key={item => Date.now() + item.id}
-      renderItem={itemData => (
-        <ImageItem
-          imageUri={itemData.item.image_url}
-          text={itemData.item.text}
-          translatedText={itemData.item.translated_text}
-          favorite={itemData.item.favorite}
-          language={itemData.item.language}
-          user_id={itemData.item.user_id}
-          onSelect={() => {
-            // props.navigation.navigate('ImageDetail', {
-            //   imageTitle: itemData.item.title,
-            //   imageId: itemData.item.id
-            // });
-            }}
-          /> 
-        )} 
-      />
+      <FlatList
+        data={myImages}
+        keyExtractor={item => item.id}
+        key={item => Date.now() + item.id}
+        renderItem={itemData => (
+          <ImageItem
+            removeImageHandlerCallback={removeImageHandler}
+            imageUri={itemData.item.image_url}
+            text={itemData.item.text}
+            translatedText={itemData.item.translated_text}
+            favorite={itemData.item.favorite}
+            language={itemData.item.language}
+            user_id={itemData.item.user_id}
+            id={itemData.item.id}
+            onSelect={() => {
+              // props.navigation.navigate('ImageDetail', {
+              //   imageTitle: itemData.item.title,
+              //   imageId: itemData.item.id
+              // });
+              }}
+            /> 
+          )} 
+        />
     </View>
-
   );
 };
 
